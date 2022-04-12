5dff8b681f0f07766478dab3382f5554047e1e96
stuarthaze <76269021+stuarthaze@users.noreply.github.com>
2022-04-13 00:54:10 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,58 @@
+# Udacity Data Engineering Project 1
+## Schema for song play analysis
+***
+## Overview 
+Create a song database called 'sparkifydb' using a star schema optimized for queries on song plays.  The database itself is implemented in PostgreSQL and interactions are performed in Python using the library psycopg2.  
+The database contains the following tables:
+
+### Fact table:
+#### `songplays`
+Column names:
+songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent
+
+### Dimension tables:
+#### `users`
+user_id, first_name, last_name, gender, level
+
+#### `songs`
+song_id, title, artist_id, year, duration
+
+#### `artists`
+artist_id, name, location, latitude, longitude
+
+#### `time`
+start_time, hour, day, week, month, year, weekday
+
+***
+## Usage
+1. Start PostgreSQL server
+2. Create role 'student' and database 'studentdb' (Required to make an initial connection)
+    - `CREATE ROLE student WITH LOGIN CREATEDB PASSWORD 'student'`
+    - `CREATE DATABASE IF NOT EXISTS studentdb`
+3. Run create_tables
+    - `python create_tables.py`
+4.  Perform the ETL (Extract Load Transform) process and fill in the database
+    - `python etl.py`
+5. Use the database to perform further data analysis. 
+    - Example: Which users have played the most songs?
+```
+SELECT songplays.user_id, users.first_name, COUNT(*) 
+    FROM songplays
+    JOIN users ON songplays.user_id = users.user_id  
+    GROUP BY songplays.user_id, users.first_name  
+    ORDER BY count DESC
+    LIMIT 10;
+```
+
+***
+## File desctiptions
+* `create_tables.py` 
+    - Clears and recreates the tables for the sparkifydb database
+* `etl.py`
+    - Main program for reading the data from files and loading the data into the database
+* `sql_queries.py` 
+    - Contains the SQL queries used to create tables and insert data
+
+***
+### NOTE
+There needs to be a lot more data added to the song library for more interesting queries.
---
+++ etl.py
@@ -0,0 +1,120 @@
+import os
+import glob
+import psycopg2
+import pandas as pd
+from sql_queries import *
+
+
+def process_song_file(cur, filepath):
+    """
+    Reads a json file containing details for a song and inserts these details into the 
+    songs and artists tables in the database 
+
+    Args:
+        cur (psycopg2.connection.cursor): Cursor which communicates with the database
+        filepath (str)
+    """
+    # open song file
+    df = pd.read_json(filepath, lines=True)
+
+    # insert song record
+    song_data = list(df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0])
+    cur.execute(song_table_insert, song_data)
+    
+    # insert artist record
+    artist_data =list(df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[0])
+    cur.execute(artist_table_insert, artist_data)
+
+
+def process_log_file(cur, filepath):
+    """
+    Reads a json file containing user activity details filters them for song plays, and updates the 
+    songplays and time tables in the database. Also adds any new users to the users table.
+
+    Args:
+        cur (psycopg2.connection.cursor): Cursor which communicates with the database
+        filepath (str)
+    """
+    # open log file
+    df = pd.read_json(filepath, lines=True)
+
+    # filter by NextSong action
+    df = df[df['page']=='NextSong']
+
+    # convert timestamp column to datetime
+    t = pd.to_datetime(df['ts'], unit='ms')
+    
+    # insert time data records
+    time_data = (t.astype(str), t.dt.hour, t.dt.day, t.dt.isocalendar().week, t.dt.month, t.dt.year, t.dt.weekday)
+    column_labels = ('timestamp', 'hour', 'day', 'week_of_year', 'month', 'year', 'weekday')
+    time_df = pd.DataFrame(time_data, index=column_labels).transpose()
+
+    for i, row in time_df.iterrows():
+        cur.execute(time_table_insert, list(row))
+
+    # load user table
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
+
+    # insert user records
+    for i, row in user_df.iterrows():
+        cur.execute(user_table_insert, row)
+
+    # insert songplay records
+    for index, row in df.iterrows():
+        
+        # get songid and artistid from song and artist tables
+        cur.execute(song_select, (row.song, row.artist, int(row.length)))
+        results = cur.fetchone()
+        
+        print(results)
+
+        if results:
+            songid, artistid = results
+        else:
+            songid, artistid = None, None
+
+        # insert songplay record
+        songplay_data = (time_df.timestamp[index], row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
+        cur.execute(songplay_table_insert, songplay_data)
+
+
+def process_data(cur, conn, filepath, func):
+    """
+    Reads the filepath, locates .json files, and passes them to func to be loaded into sparkifydb.
+
+    Args:
+        cur (psycopg2.connection.cursor): Cursor which communicates with the database
+        conn (psycopg2.connection)
+        filepath (str): Root directory of the search tree
+        func (process_song_file / process_log_file): Choose the function to use
+    """
+    # get all files matching extension from directory
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root,'*.json'))
+        for f in files :
+            all_files.append(os.path.abspath(f))
+
+    # get total number of files found
+    num_files = len(all_files)
+    print('{} files found in {}'.format(num_files, filepath))
+
+    # iterate over files and process
+    for i, datafile in enumerate(all_files, 1):
+        func(cur, datafile)
+        conn.commit()
+        print('{}/{} files processed.'.format(i, num_files))
+
+
+def main():
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    cur = conn.cursor()
+
+    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
+    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
