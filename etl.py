c7a64bac2a9c078133bd3679cf0f11140083e6b0
stuarthaze <stu.to.u@gmail.com>
2022-04-13 01:08:06 +0200

    Added data folders

--- etl.py
+++ etl.py
@@ -65,8 +65,6 @@ def process_log_file(cur, filepath):
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, int(row.length)))
         results = cur.fetchone()
-        
-        print(results)
 
         if results:
             songid, artistid = results
