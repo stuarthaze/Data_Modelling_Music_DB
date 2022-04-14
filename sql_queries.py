d9521598c2412770993ffc3449f70d55f2bc2b2d
stuarthaze <stu.to.u@gmail.com>
2022-04-14 21:24:58 +0200

    Added more NOT NULL conditions

--- sql_queries.py
+++ sql_queries.py
@@ -10,22 +10,22 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS songplays (songplay_id serial PRIMARY KEY, 
-                                      start_time timestamp, 
-                                      user_id int, 
-                                      level varchar, 
+                                      start_time timestamp NOT NULL, 
+                                      user_id int NOT NULL, 
+                                      level varchar,
                                       song_id varchar, 
                                       artist_id varchar, 
-                                      session_id int, 
+                                      session_id int NOT NULL, 
                                       location varchar, 
                                       user_agent varchar)
 """)
 
 user_table_create = ("""
 CREATE TABLE IF NOT EXISTS users (user_id int PRIMARY KEY,
-                                  first_name varchar,
-                                  last_name varchar, 
+                                  first_name varchar NOT NULL,
+                                  last_name varchar NOT NULL, 
                                   gender char,
-                                  level varchar)
+                                  level varchar NOT NULL)
 """)
 
 song_table_create = ("""
@@ -46,12 +46,12 @@ CREATE TABLE IF NOT EXISTS artists (artist_id varchar PRIMARY KEY,
 
 time_table_create = ("""
 CREATE TABLE IF NOT EXISTS time (start_time timestamp PRIMARY KEY, 
-                                 hour int, 
-                                 day int, 
-                                 week int, 
-                                 month int, 
-                                 year int, 
-                                 weekday int)
+                                 hour int NOT NULL, 
+                                 day int NOT NULL, 
+                                 week int NOT NULL, 
+                                 month int NOT NULL, 
+                                 year int NOT NULL, 
+                                 weekday int NOT NULL)
 """)
 
 # INSERT RECORDS
